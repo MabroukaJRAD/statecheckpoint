fd7b43f7422ee8763bf7ad4705b344ca7e38814d
Mabrouka JRAD <mabrouka.jrad@gmail.com>
2021-12-12 12:14:59 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import myImage from './imgSrc.jpg';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+class App extends React.Component {
+  
+  state = {
+    Person: {
+      fullName : "Mabrouka JRAD",
+      bio : "I love coding",
+      imgSrc : myImage,
+      profession : "software engineer"
+    },
+    count: 0,
+    show : false
+  };
+  showPerson= () => {
+    this.setState({
+      show: !this.state.show, 
+    }
+    );
+    setInterval(()=> {
+      this.setState({
+        count:this.state.count+1
+      })
+      console.log(this.state.count);
+        },2000); 
+        this.setState({
+          count:0,
+        })
+       
+      };
+  
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1> hello</h1>
+      {this.state.show &&  (
+          <>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="flowers"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <h1>{this.state.count}</h1>
 
+            <br></br>
+          </>
+        )}
+      <Button variant="outline-primary" onClick={this.showPerson}>click here</Button>
+           </div>
+  );
+  
+}
+}
 export default App;
